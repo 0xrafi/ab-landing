636a6f5f6997012bade3db427a2b9dd0c12961bc
Rafi <rafi337161@gmail.com>
2022-04-08 21:53:38 -0500

    removed zola script and added hardcoded button and image(s)

--- src/pages/Registry.js
+++ src/pages/Registry.js
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react';
-import {loadZolaRegistry} from '../scripts/loadZola'
-function Registry() {
-    useEffect(() => {
-        loadZolaRegistry();
-    }, []);
+import React from 'react';
 
+import registry from '../img/registry.jpeg';
+import zolaLogo from '../img/zolalogo.jpeg';
+
+function Registry() {
     return (
-        <div className='textContent'>
-            <a className="zola-registry-embed" href="https://www.zola.com/registry/abinfinity" data-registry-key="abinfinity"/>
-        </div>
+        <div className='rsvpPage'>
+            <img src={registry} className="normalPic" />
+            <br></br>
+            <img src={zolaLogo} className="zolaLogo" />
+            <br></br>
+            <div className='buttonWrapper'>
+                <a href="https://www.zola.com/registry/collection-item/624df2776af8ff28c43ab13b" target="_blank">
+                    <button type='submit'>CONTRIBUTE TO OUR NEWLYWED FUND</button>
+                </a>
+            </div>
+        </div >
     );
 }
 
