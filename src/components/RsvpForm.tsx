3ed8d3c0d58dd298e8705ef60cfacb0c6a110d14
Rafi <rafi337161@gmail.com>
2022-04-09 16:50:53 -0500

    saving work on refactor to rsvpform

---
+++ src/components/RsvpForm.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+interface Props {
+  email: string;
+  buildOptions: () => {};
+}
+
+function RsvpForm({ email, buildOptions }: Props) {
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const form = document.getElementById('my-form');
+        const action = event.target.action;
+        const data = new FormData(form as HTMLFormElement);
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                alert("You have successfully RSVPed!");
+                window.location.href = "/";
+            })
+    }
+
+  return (
+    <form
+      method="POST"
+      action="https://script.google.com/macros/s/AKfycbzKWk4MUWW_tqA9ji5kR_kGnem7eRv1BJXlfuOLJHwQa4uMlqL60KyvjKzAzh1-WTs5/exec"
+      id="my-form"
+      onSubmit={onSubmit}
+    >
+      <div id="hide">
+        <input name="Email" value={email} type="text" required />
+      </div>
+      <div>
+        <label>Name *</label>
+        <input name="Name" type="text" required />
+      </div>
+      <br></br>
+      <div>
+        <label>How many guests will be in your party? *</label>
+        <select name="NumberOfGuests">{buildOptions()}</select>
+      </div>
+      <br></br>
+      <div>
+        <label>Are you attending the wedding? *</label>
+        <select name="AttendingWedding">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+      </div>
+      <br></br>
+      <div>
+        <label>Are you attending the concert? *</label>
+        <select name="AttendingConcert">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+      </div>
+      <br></br>
+      <div>
+        <label>Are you attending the reception? *</label>
+        <select name="AttendingReception">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+      </div>
+      <br></br>
+      <div className="buttonWrapper">
+        <button type="submit">RSVP</button>
+      </div>
+    </form>
+  );
+}
+
+export default RsvpForm;
--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 const Papa = require("papaparse");
 
 import rsvpPic from '../img/ABRsvpEngagementPic.jpeg';
+import RsvpForm from '../components/RsvpForm.tsx';
 
 const parseFile = () => new Promise((resolve) => {
     Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vRpdEblJlslYiGTcfGCv4keTIYrHg_aceu_iiPalEfXS9VM_IKiIFPqlUN8YEbYwQkWK5qdP0ZwDqcZ/pub?output=csv", {
@@ -44,20 +45,20 @@ function RSVP() {
         return arr;
     }
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        const form = document.getElementById('my-form');
-        const action = event.target.action;
-        const data = new FormData(form);
-        fetch(action, {
-          method: 'POST',
-          body: data,
-        })
-        .then(() => {
-          alert("You have successfully RSVPed!");
-          window.location.href = "/";
-        })
-    }
+    // const onSubmit = (event) => {
+    //     event.preventDefault();
+    //     const form = document.getElementById('my-form');
+    //     const action = event.target.action;
+    //     const data = new FormData(form);
+    //     fetch(action, {
+    //         method: 'POST',
+    //         body: data,
+    //     })
+    //         .then(() => {
+    //             alert("You have successfully RSVPed!");
+    //             window.location.href = "/";
+    //         })
+    // }
 
     return (
         <div className='rsvpPage'>
@@ -65,57 +66,7 @@ function RSVP() {
             Please RSVP by April 15th, 2022.<br></br><br></br>
 
             {(password == "ABLA2022" || password == "abla2022") && getGuestFromEmail(email) ?
-                <form method="POST" action="https://script.google.com/macros/s/AKfycbzKWk4MUWW_tqA9ji5kR_kGnem7eRv1BJXlfuOLJHwQa4uMlqL60KyvjKzAzh1-WTs5/exec" id="my-form" onSubmit={onSubmit}>
-                    <div id="hide">
-                        <input
-                            name='Email'
-                            value={email}
-                            type='text'
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label>Name *</label>
-                        <input
-                            name='Name'
-                            type='text'
-                            required
-                        />
-                    </div>
-                    <br></br>
-                    <div>
-                        <label>How many guests will be in your party? *</label>
-                        <select name="NumberOfGuests">
-                            {buildOptions()}
-                        </select>
-                    </div>
-                    <br></br>
-                    <div>
-                        <label>Are you attending the wedding? *</label>
-                        <select name="AttendingWedding">
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-                    <br></br>
-                    <div>
-                        <label>Are you attending the concert? *</label>
-                        <select name="AttendingConcert">
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-                    <br></br>
-                    <div>
-                        <label>Are you attending the reception? *</label>
-                        <select name="AttendingReception">
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-                    <br></br>
-                    <div className='buttonWrapper'><button type="submit">RSVP</button></div>
-                </form> :
+                <RsvpForm email={email} buildOptions={buildOptions} /> :
                 <div>
                     <label>Enter the password:</label>
                     <input
